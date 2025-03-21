8b2c55ce450d8de1be6babc26988d07c526c86f4
Eduardosebastiaomatondo7x <eduardosebastiaomatondo@gmail.com>
2025-03-21 09:24:24 +0100

    valide

--- main.js
+++ main.js
@@ -1,17 +1,31 @@
 AFRAME.registerComponent('toggleclick', {
   init: function () {
-    this.onClick = this.onClick.bind(this)
-    this.el.addEventListener("click", this.onClick)
+    this.onClick = this.onClick.bind(this);
+    this.el.addEventListener("click", this.onClick);
   },
 
   onClick: async function() {
-    let ok = document.querySelector(this.el.getAttribute("src"))
-    this.el.object3D.visible = !this.el.object3D.visible
-    if (this.el.object3D.visible) {
-      ok.play()
+    let mediaEl = document.querySelector(this.el.getAttribute("src"));
+    
+    if (mediaEl) {
+      // Basculer la visibilité de l'élément
+      this.el.object3D.visible = !this.el.object3D.visible;
+
+      // Contrôler la lecture en fonction de la visibilité
+      if (this.el.object3D.visible) {
+        // Assurer que c'est un élément vidéo ou audio avant de jouer
+        if (mediaEl.tagName === "VIDEO" || mediaEl.tagName === "AUDIO") {
+          mediaEl.play();
+        }
+      } else {
+        if (mediaEl.tagName === "VIDEO" || mediaEl.tagName === "AUDIO") {
+          mediaEl.pause();
+          mediaEl.currentTime = 0;  // Remettre à zéro le temps pour redémarrer
+        }
+      }
     } else {
-      ok.pause()
-      ok.currentTime = 0
+      console.warn("Elément source non trouvé :", this.el.getAttribute("src"));
     }
   }
 });
+
